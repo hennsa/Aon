290d75d571d2fedb1cc442d34adea4ab67ef6991
hennsa <hennsa@outlook.com>
2026-01-16 23:27:05 +0100

    Add initial book JSON schema

---
+++ Aon/Aon/src/Aon.Content/FrontMatterSection.cs
@@ -0,0 +1,8 @@
+namespace Aon.Content;
+
+public sealed class FrontMatterSection
+{
+    public string Id { get; init; } = string.Empty;
+    public string Title { get; init; } = string.Empty;
+    public string Html { get; init; } = string.Empty;
+}
