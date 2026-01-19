3cc654b39acf326bf9f094dab9c7c250ed703d61
Carlo <hennsa@outlook.com>
2026-01-19 13:46:04 +0100

    Local changes

--- Aon/Aon/src/Aon.Desktop.Wpf/ViewModels/ItemEntryViewModel.cs
+++ Aon/Aon/src/Aon.Desktop.Wpf/ViewModels/ItemEntryViewModel.cs
@@ -1,13 +1,27 @@
 namespace Aon.Desktop.Wpf.ViewModels;
 
-public sealed class ItemEntryViewModel
+public sealed class ItemEntryViewModel : ViewModelBase
 {
+    private string _name;
+    private string _category;
+
     public ItemEntryViewModel(string name, string category)
     {
-        Name = name;
-        Category = category;
+        _name = name;
+        _category = category;
     }
 
-    public string Name { get; }
-    public string Category { get; }
+    // Make Name writable so TwoWay bindings work
+    public string Name
+    {
+        get => _name;
+        set => SetProperty(ref _name, value);
+    }
+
+    // Make Category writable as well (keeps behaviour consistent)
+    public string Category
+    {
+        get => _category;
+        set => SetProperty(ref _category, value);
+    }
 }
